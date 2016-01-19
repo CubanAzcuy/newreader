068128b1c2d504cac5a9f109051122db71bc1d14
Robert Gross <mrgross17@gmail.com>
2016-01-19 15:11:46 -0500

    Created Realm Object

--- Android/NewsReader/app/src/main/java/mmilord/newsreader/core/models/Article.java
+++ Android/NewsReader/app/src/main/java/mmilord/newsreader/core/models/Article.java
@@ -2,21 +2,25 @@ package mmilord.newsreader.core.models;
 
 import com.google.gson.annotations.SerializedName;
 
+import io.realm.RealmObject;
+
 /**
  * Created by robertgross on 1/9/16.
  */
 public class Article {
 
     @SerializedName("url")
-    String _url;
+    private String _url;
 
     @SerializedName("title")
-    String _title;
+    private String _title;
 
     @SerializedName("imageURL")
-    String _imageURL;
+    private String _imageURL;
 
     @SerializedName("contents")
-    String _contents;
+    private String _contents;
+
+
 
 }
